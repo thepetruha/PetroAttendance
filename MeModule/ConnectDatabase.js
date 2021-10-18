ba99dc2ec407ba013b433e9657884b260b766b4e
antonkucher1 <kucher.antosha@mail.ru>
2021-10-19 00:33:03 +0300

    probel

--- MeModule/ConnectDatabase.js
+++ MeModule/ConnectDatabase.js
@@ -11,7 +11,7 @@ try {
     sequelize.authenticate();
     console.log('Connection has been established successfully.');
 } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.error('Unable to connect to the database:', error); 
 }
 
 //---------------------------------------------------------------------------
